280d860c0a5d8de729a91897b0c69727862f646d
Akcio23 <114962591+Akcio23@users.noreply.github.com>
2024-08-15 22:26:21 -0300

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ axios(url)
   .then(response => {
     const html = response.data; //extraindo o HTML
     const $ = cheerio.load(html); //Carregando o html 
-    const newspaper  = $('h2 > a > p').length //fitrando e realizado a contagem por TAG 
+    const newspaper  = $('h2 > a > p').length //filtrando e realizado a contagem por TAG 
     for(i=0; i <= newspaper; i++){
         console.log($(`h2 > a > p:eq(${i})`).text());
     }
